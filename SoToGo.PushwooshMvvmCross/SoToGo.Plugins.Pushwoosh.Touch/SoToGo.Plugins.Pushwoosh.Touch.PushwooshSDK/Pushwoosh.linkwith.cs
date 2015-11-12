c0afb83a0e5e6118a06b59db56dad39af9e6042f
Jelle Damen <j.damen@softwaretogo.nl>
2015-11-12 20:53:12 +0100

    Added Android and iOS submodules

---
+++ SoToGo.PushwooshMvvmCross/SoToGo.Plugins.Pushwoosh.Touch/SoToGo.Plugins.Pushwoosh.Touch.PushwooshSDK/Pushwoosh.linkwith.cs
@@ -0,0 +1,4 @@
+﻿using System;
+using ObjCRuntime;
+
+[assembly: LinkWith ("Pushwoosh", LinkTarget.Arm64 | LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, ForceLoad = true, Frameworks = "CoreGraphics CoreFoundation Security StoreKit")]
---
+++ SoToGo.PushwooshMvvmCross/SoToGo.Plugins.Pushwoosh.Touch/SoToGo.Plugins.Pushwoosh.Touch.PushwooshSDK/StructsAndEnums.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SoToGo.Plugins.Pushwoosh.Touch.PushwooshSDK
+{
+	public enum PWSupportedOrientations {
+		OrientationPortrait = 1 << 0,
+		OrientationPortraitUpsideDown = 1 << 1,
+		OrientationLandscapeLeft = 1 << 2,
+		OrientationLandscapeRight = 1 << 3
+	}
+}
+
